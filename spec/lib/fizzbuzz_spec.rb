aa79d792cd88bbdf1f637e56b6c115d8814a422e
Alex-Swann <alex_swann@hotmail.co.uk>
2016-05-16 12:53:40 +0100

    2nd test fail

--- spec/lib/fizzbuzz_spec.rb
+++ spec/lib/fizzbuzz_spec.rb
@@ -1,7 +1,15 @@
 require 'fizzbuzz'
 
 describe 'fizzbuzz' do
+
   it 'returns fizz when 3' do
     expect(fizzbuzz(3)).to eq 'fizz'
   end
+
+  it  'returns 5 when 5' do
+    expect(fizzbuzz(5)).to eq '5'
+  end
+
+
 end
+
