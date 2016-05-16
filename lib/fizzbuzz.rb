dd1121c793e7b3fa8aa1fb2f7f706e9d865bb89a
Elena <elena.vilimaite@gmail.com>
2016-05-16 13:41:14 +0100

    all tests passing

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,5 +1,20 @@
 def fizzbuzz(number)
-  return 'fizzbuzz' if number % 15 == 0
-  return number.to_s if number != 3 && number != 5
-	number == 5 ? 'buzz' : 'fizz'
+	if number % 3 == 0
+		if number % 5 == 0
+			return 'fizzbuzz'
+		else
+			return 'fizz'
+		end
+	elsif number % 5 == 0
+		return 'buzz'
+	else
+		return number.to_s
+	end
+		
+
+	# return 'fizzbuzz' if number % 5 == 0 && number % 3 == 0 && number % 15 == 0
+	# return 'fizz' if number % 3 == 0
+	# return 'buzz' if number % 5 == 0
+	# return number.to_s if number != 3 && number != 5
+
 end
--- spec/lib/fizzbuzz_spec.rb
+++ spec/lib/fizzbuzz_spec.rb
@@ -17,13 +17,13 @@ describe 'fizzbuzz' do
 	it "makes you solve the whole test because i'm an arse" do
 		a = [*1..10000]
 		a.each do |n|
-      if n % 3 == 0
-        expect(fizzbuzz(n)).to eq 'fizz'
-      elsif n % 5 == 0 && n % 15 != 0
-        expect(fizzbuzz(n)).to eq 'fizz'
-      else
-        expect(fizzbuzz(n)).to eq n.to_s unless n % 15 == 0
-      end
+			if n % 3 == 0 && n % 15 != 0
+				expect(fizzbuzz(n)).to eq 'fizz'
+			elsif n % 5 == 0 && n % 15 != 0
+				expect(fizzbuzz(n)).to eq 'buzz'
+			else
+				expect(fizzbuzz(n)).to eq n.to_s unless n % 15 == 0
+			end
 		end
 	end
 
@@ -31,6 +31,11 @@ describe 'fizzbuzz' do
 		expect(fizzbuzz(15)).to eq 'fizzbuzz'
 	end
 
-
+	it "tests for fizzbuzz" do
+		mod5 = [*1..10000].select {|i | i % 15 == 0 }
+		mod5.each do |n|
+			expect(fizzbuzz(n)).to eq 'fizzbuzz'
+		end
+	end
 end
 
