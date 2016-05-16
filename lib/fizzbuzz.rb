2042991a55bdbac2ce307c726dbed3806d7ca05a
Elena <elena.vilimaite@gmail.com>
2016-05-16 13:15:22 +0100

    Your tests are bad, you should feel bad

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,4 +1,4 @@
 def fizzbuzz(number)
-  return '2' if number == 2
+  return number.to_s if number != 3 && number != 5
 	number == 5 ? 'buzz' : 'fizz'
 end
--- spec/lib/fizzbuzz_spec.rb
+++ spec/lib/fizzbuzz_spec.rb
@@ -2,25 +2,27 @@ require 'fizzbuzz'
 
 describe 'fizzbuzz' do
 
-  it 'returns fizz when 3' do
-    expect(fizzbuzz(3)).to eq 'fizz'
-  end
+	it 'returns fizz when 3' do
+		expect(fizzbuzz(3)).to eq 'fizz'
+	end
 
-  it  'returns buzz when 5' do
-    expect(fizzbuzz(5)).to eq 'buzz'
-  end
+	it  'returns buzz when 5' do
+		expect(fizzbuzz(5)).to eq 'buzz'
+	end
 
-  it  'returns 2 when 2' do
-    expect(fizzbuzz(2)).to eq '2'
-  end
+	it  'returns 2 when 2' do
+		expect(fizzbuzz(2)).to eq '2'
+	end
 
-  it 'returns all numbers in string format when not 3,5,15' do
-    a = [*1..10000]
-    r = a.select{|i| i % 3 != 0 && i % 5 != 0 }
-    r.each do |n|
-      expect(fizzbuzz(n)).to eq n.to_s
-    end
-  end
-
+	it 'returns all numbers in string format when not 3,5,15' do
+		a = [*1..10000]
+		r = a.select{|i| i % 3 != 0 && i % 5 != 0 }
+		r.each do |n|
+			expect(fizzbuzz(n)).to eq n.to_s
+		end
+	end
+	it 'returns fizzbuzz when 15 is passed' do
+		expect(fizzbuzz(15)).to eq 'fizzbuzz'
+	end
 end
 
