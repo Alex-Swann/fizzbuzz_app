3b90d1cf46255fe3b27a9d1b78b7db4106a5c7d4
Elena <elena.vilimaite@gmail.com>
2016-05-17 10:26:13 +0100

    edge cases covered

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,21 +1,22 @@
 def fizzbuzz(number)
-	if number % 3 == 0
-		if number % 5 == 0
-			return 'fizzbuzz'
+	if number.is_a? Integer
+		if number.between?(1, 100)
+			if number % 3 == 0
+				if number % 5 == 0
+					return 'fizzbuzz'
+				else
+					return 'fizz'
+				end
+			elsif number % 5 == 0
+				return 'buzz'
+			else
+				return number.to_s
+			end
 		else
-			return 'fizz'
+			return "Integer outside of range. Try again!"
 		end
-	elsif number % 5 == 0
-		return 'buzz'
 	else
-		return number.to_s
+		return "Fail! Not an integer"
 	end
 
-
-	# return 'fizzbuzz' if number % 5 == 0 && number % 3 == 0 && number % 15 == 0
-	# return 'fizz' if number % 3 == 0
-	# return 'buzz' if number % 5 == 0
-	# return number.to_s if number != 3 && number != 5
-
 end
-
