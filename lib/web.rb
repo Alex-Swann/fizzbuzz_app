e134c029c73cf2cac94819d911b68779ca5b15e1
Elena <elena.vilimaite@gmail.com>
2016-05-18 00:26:21 +0100

    basic web app

--- lib/web.rb
+++ lib/web.rb
@@ -1,5 +1,17 @@
+require 'rubygems'
 require 'sinatra'
+require 'haml'
+
+require_relative './fizzbuzz'
+
 
 get '/' do
-  "Hello, world"
+	haml :index
 end
+
+post '/fizzbuzz' do
+	haml :fizzbuzz, :locals => {:fzbz => fizzbuzz(params[:number].to_i)}
+end
+
+
+# , :locals => {:some_object => fizzbuzz(3)}
